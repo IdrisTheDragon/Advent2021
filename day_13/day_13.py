389363fe4fbb5bdca785c9df28ec49f11a6284f7
IdrisTheDragon <nathaniwilliamsuk@gmail.com>
2021-12-13 07:31:36 +0000

    day_13

---
+++ day_13/day_13.py
@@ -0,0 +1,71 @@
+
+
+file = open("input.txt","r")
+
+grid = []
+folds = []
+c = True
+for l in file:
+    if l == "\n":
+        c = False
+    elif c:
+        s = l[:-1].split(",")
+        grid.append( (int(s[0]),int(s[1])) )
+    else:
+        s = l[11:-1].split("=")
+        folds.append((s[0],int(s[1])))
+print(grid)
+print(folds)
+
+def fold(f,grid):
+    temp = []
+    for (x,y) in grid:
+        if f[0] == 'y':
+            p = f[1]
+            if p < y:
+                dif = y - p
+                newY = p - dif
+                temp.append((x,newY))
+            else:
+                temp.append((x,y))
+        elif f[0] == 'x':
+            p = f[1]
+            if p < x:
+                dif = x - p
+                newX = p - dif
+                temp.append((newX,y))
+            else:
+                temp.append((x,y))
+    return temp
+
+grid1 = fold(folds[0],grid)
+unique = set(grid1)
+print(unique)
+print(len(unique))
+
+for f in folds:
+    grid = fold(f,grid)
+
+def get_x(a):
+    return a[0]
+
+def get_y(a):
+    return a[1]
+
+
+x = list(map(get_x,grid))
+y = list(map(get_y,grid))
+
+maxX = max(x)
+maxY = max(y)
+
+printGrid = [["." for i in range(maxX+1)] for j in range(maxY+1)]
+
+for c in grid:
+    printGrid[c[1]][c[0]] = "#"
+
+for row in printGrid:
+    r = ""
+    for m in row:
+        r = r+m
+    print(r)
